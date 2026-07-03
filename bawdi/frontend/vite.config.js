0e923cba4c085c1dfe2331aa2b19af38d7f3c077
AdrianARPM <164609550+AdrianARPM@users.noreply.github.com>
2026-07-03 22:51:56 +0700

    Change PWA registration type to 'prompt'

--- bawdi/frontend/vite.config.js
+++ bawdi/frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['icons/*.png', 'icons/*.svg'],
       manifest: {
         name: 'BAWDI Maintenance System',
